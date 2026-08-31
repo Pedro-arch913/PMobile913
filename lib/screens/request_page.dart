b1e1c3cb065d0e25ee95c7cc7992210868c17b20
Carlos Gardel <cgga1@aluno.ifal.edu.br>
2026-08-31 17:26:43 -0300

    17:27 - 31/08/2026 - Pulling from 'master'

--- lib/screens/request_page.dart
+++ lib/screens/request_page.dart
@@ -1,33 +1,31 @@
-  //if it starts with an uppercase letter, it's a Widget
-  //if it starts with a downcase letter, it's an argument
-  //arguments go in Widgets, widgets go in arguments
-  //The child is what is inside the Widget
+//if it starts with an uppercase letter, it's a Widget
+//if it starts with a downcase letter, it's an argument
+//arguments go in Widgets, widgets go in arguments
+//The child is what is inside the Widget
 
- import 'package:flutter/material.dart';
- import 'package:pmobile913/screens/confirm_page.dart';
+import 'package:flutter/material.dart';
+import 'package:pmobile913/screens/confirm_page.dart';
 import 'package:pmobile913/screens/detail_page.dart';
-  import 'package:pmobile913/domain/dados.dart';
+import 'package:pmobile913/domain/dados.dart';
 
- class HomePage extends StatefulWidget {
-   const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-   @override
-   State<HomePage> createState() => _HomePageState();
- }
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
- class _HomePageState extends State<HomePage> {
-   int currentPage = 0;
+class _HomePageState extends State<HomePage> {
+  int currentPage = 0;
 
-   @override
-
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: Icon(Icons.arrow_back),
-        title: const Text('Pedido de Sr. Manoel',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-          ),
+        title: const Text(
+          'Pedido de Sr. Manoel',
+          style: TextStyle(fontWeight: FontWeight.bold),
         ),
         backgroundColor: Colors.white10,
       ),
@@ -35,25 +33,24 @@ import 'package:pmobile913/screens/detail_page.dart';
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Container(
-            child: Text('Imagem:',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
+            child: Text(
+              'Imagem:',
+              style: TextStyle(fontWeight: FontWeight.bold),
             ),
             padding: EdgeInsets.all(4),
           ),
           Card(
-            child:
-            Image.asset('assets/imagens/download.jpg',
+            child: Image.asset(
+              'assets/imagens/download.jpg',
               width: 400,
               height: 200,
             ),
           ),
           Container(
-            child: Text('Informações Gerais:',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),),
+            child: Text(
+              'Informações Gerais:',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
           ),
           Container(
             margin: EdgeInsets.all(16),
@@ -64,19 +61,16 @@ import 'package:pmobile913/screens/detail_page.dart';
             ),
             child: Text(
               'Estou precisando da ajuda de algum profissional '
-                  'que consiga fazer manutenção em disjuntores elétricos '
-                  'residenciais.',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.white,
-              ),
+              'que consiga fazer manutenção em disjuntores elétricos '
+              'residenciais.',
+              style: TextStyle(fontSize: 16, color: Colors.white),
             ),
           ),
           Container(
-            child: Text('Informações Complementares:',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),),
+            child: Text(
+              'Informações Complementares:',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
           ),
           Container(
             margin: EdgeInsets.all(16),
@@ -87,29 +81,37 @@ import 'package:pmobile913/screens/detail_page.dart';
             ),
             child: Text(
               'Tenho algumas informações adicionais: a primeira é '
-                  'que eu gostaria que o serviço fosse feito durante a manhã, '
-                  'pois é o horário que trabalho fora de casa por mais tempo e '
-                  'não uso os eletrodomésticos. Pagarei um adicional extra se '
-                  'já estiver pronto quando eu voltar para o almoço.',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.white,
-              ),
+              'que eu gostaria que o serviço fosse feito durante a manhã, '
+              'pois é o horário que trabalho fora de casa por mais tempo e '
+              'não uso os eletrodomésticos. Pagarei um adicional extra se '
+              'já estiver pronto quando eu voltar para o almoço.',
+              style: TextStyle(fontSize: 16, color: Colors.white),
             ),
-          )
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.orange,
         onPressed: () {
-          Navigator.push(context, MaterialPageRoute(
-              builder: (context) => DetailPage()
-          ),
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => DetailPage(
+                dados: Dados(
+                  urlImagem: 'assets/imagens/download.jpg',
+                  informacoes:
+                      'Estou precisando da ajuda de algum profissional '
+                      'que consiga fazer manutenção em disjuntores elétricos '
+                      'residenciais.',
+                  nome: 'Sr. Manoel',
+                  avaliacao: '4.5',
+                ),
+              ),
+            ),
           );
         },
         child: const Icon(Icons.check),
       ),
-
     );
   }
 }

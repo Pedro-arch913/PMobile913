393e0fe79ddd686acf2fdf3993529b7b870730d5
Carlos Gardel <cgga1@aluno.ifal.edu.br>
2026-09-07 10:40:13 -0300

    10:40 - 07/09/2026 - Corrigindo algumas coisas

--- lib/screens/historico_detail.dart
+++ lib/screens/historico_detail.dart
@@ -1,7 +1,5 @@
-import 'package:pmobile913/db/db_helper.dart';
 import 'package:pmobile913/domain/historico.dart';
 import 'package:flutter/material.dart';
-
 
 class HistoricoDetail extends StatefulWidget {
   Historico historico;
@@ -16,32 +14,26 @@ class _HistoricoDetailState extends State<HistoricoDetail> {
   @override
   Historico get historico => widget.historico;
 
-
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(centerTitle: false,
-        title: Text('Detalhes do serviço',
-          style: TextStyle(
-              color: Colors.white,
-              fontSize: 24
-          ),
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text(
+          'Detalhes do serviço',
+          style: TextStyle(color: Colors.white, fontSize: 24),
         ),
-        backgroundColor: Color(0xFF2d2d2d
-        ),
+        backgroundColor: Color(0xFF2d2d2d),
       ),
       backgroundColor: Colors.black,
       body: ListView(
         children: [
-          Stack(
-            children: [
-              Image.network(historico.imagem),
-            ],
-          )
+          Stack(children: [Image.network(historico.imagem)]),
         ],
       ),
     );
   }
-  buildContainer({required Historico historico}){
+
+  buildContainer({required Historico historico}) {
     return Container(
       margin: EdgeInsets.all(25),
       child: Column(
@@ -49,39 +41,34 @@ class _HistoricoDetailState extends State<HistoricoDetail> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
 
         children: [
-        Stack(
-        fit: StackFit.passthrough,
-        children: [
-          ClipRRect(
-            child: Image.network(
-                historico.imagem, height: 20, fit: BoxFit.cover),
-            borderRadius: BorderRadius.circular(8),
-          ),
-        ],
-      ),
-      SizedBox(height: 8),
           Stack(
-              children: [
-                Text(widget.historico.servico,
-                style: TextStyle(
-                color: Colors.white,
-                fontSize: 16
-                 ),
-              ),
-                Text(widget.historico.preco,
-                  style: TextStyle(
-                      color: Colors.green,
-                      fontSize: 16
-                  ),
+            fit: StackFit.passthrough,
+            children: [
+              ClipRRect(
+                child: Image.network(
+                  historico.imagem,
+                  height: 20,
+                  fit: BoxFit.cover,
                 ),
-            ]
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ],
+          ),
+          SizedBox(height: 8),
+          Stack(
+            children: [
+              Text(
+                widget.historico.servico,
+                style: TextStyle(color: Colors.white, fontSize: 16),
+              ),
+              Text(
+                widget.historico.preco,
+                style: TextStyle(color: Colors.green, fontSize: 16),
+              ),
+            ],
           ),
         ],
       ),
-
     );
-
   }
-
-
 }
